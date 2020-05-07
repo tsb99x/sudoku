67bd642c354e24c5d2c47d5b472c5b460ff4db40
Anton Muravyov <tsb99x@gmail.com>
2020-05-07 21:56:24 +0300

    Simple draw, hover and click

--- src/app/main.c
+++ src/app/main.c
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
+#define TILE_SIZE 50
+#define TILE_GAP 3
+
 int WinMain(
         HINSTANCE hInstance,
         HINSTANCE hPrevInstance,
@@ -11,7 +14,10 @@ int WinMain(
 ) {
         SDL_Window *window;
         SDL_Surface *surface;
-        int res = 0;
+        SDL_Rect rect;
+        SDL_Event event;
+        int x, y, mouse_x, mouse_y, mouse_bt, color;
+        int res = 0, quit = 0;
 
         if (SDL_Init(SDL_INIT_VIDEO)) {
                 SDL_Log("Failed to init SDL: %s\n", SDL_GetError());
@@ -32,9 +38,48 @@ int WinMain(
         }
 
         surface = SDL_GetWindowSurface(window);
-        SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-        SDL_UpdateWindowSurface(window);
-        SDL_Delay(3000);
+
+        while (!quit) {
+                while (SDL_PollEvent(&event)) {
+                        if (event.type == SDL_QUIT)
+                                quit = 1;
+                        if (event.type == SDL_KEYDOWN)
+                                quit = 1;
+                }
+
+                SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+                mouse_bt = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+                for (x = 0; x < 9; ++x)
+                        for (y = 0; y < 9; ++y) {
+                                rect.x = x * (TILE_SIZE + TILE_GAP);
+                                if (x >= 3)
+                                        rect.x += TILE_GAP * 2;
+                                if (x >= 6)
+                                        rect.x += TILE_GAP * 2;
+                                rect.y = y * (TILE_SIZE + TILE_GAP);
+                                if (y >= 3)
+                                        rect.y += TILE_GAP * 2;
+                                if (y >= 6)
+                                        rect.y += TILE_GAP * 2;
+                                rect.w = rect.h = TILE_SIZE;
+
+                                color = SDL_MapRGB(surface->format, 0xFF, 0, 0xFF);
+
+                                if (mouse_x >= rect.x && mouse_x <= rect.x + rect.w && mouse_y >= rect.y && mouse_y <= rect.y + rect.h)
+                                        if (SDL_BUTTON(mouse_bt) == SDL_BUTTON_LEFT)
+                                                color = SDL_MapRGB(surface->format, 0, 0, 0xFF);
+                                        else
+                                                color = SDL_MapRGB(surface->format, 0, 0xFF, 0);
+
+                                SDL_FillRect(surface, &rect, color);
+                        }
+
+                SDL_UpdateWindowSurface(window);
+        }
+
+        SDL_Log("Exit!\n");
 
         SDL_DestroyWindow(window);
 cleanup_sdl:
