8564843a9af1a60603674916f2c8a4b1f802b3d7
Anton Muravyov <tsb99x@gmail.com>
2020-05-11 22:30:40 +0300

    Color scheme, Digits rendering, Button struct

--- src/app/main.c
+++ src/app/main.c
@@ -7,6 +7,108 @@
 #define TILE_SIZE 50
 #define TILE_GAP 3
 
+/* Simplify Digit Drawing by Caching */
+
+SDL_Texture *digits_cache[9];
+
+static void buffer_digits(
+        SDL_Renderer *renderer,
+        TTF_Font *font,
+        SDL_Color *color,
+        SDL_Texture *texs[]
+) {
+        SDL_Surface *surface;
+        int i;
+        char sz[2] = "i";
+
+        for (i = 0; i < 9; ++i) {
+                sz[0] = '0' + i + 1;
+                surface = TTF_RenderUTF8_Blended(font, sz, *color);
+                texs[i] = SDL_CreateTextureFromSurface(renderer, surface);
+                SDL_FreeSurface(surface);
+        }
+}
+
+static void destroy_digits(
+        SDL_Texture *texs[]
+) {
+        int i;
+
+        for (i = 0; i < 9; ++i) {
+                SDL_DestroyTexture(texs[i]);
+        }
+}
+
+/* Color Scheme Definition */
+
+SDL_Color white       = { 0xFF, 0xFF, 0xFF, 0xFF };
+SDL_Color dark_green  = { 0x66, 0x99, 0x22, 0xFF };
+SDL_Color light_green = { 0x99, 0xCC, 0x33, 0xFF };
+SDL_Color orange      = { 0xFF, 0x60, 0x00, 0xFF };
+SDL_Color black       = { 0x00, 0x00, 0x00, 0xFF };
+
+/* Color Scheme Binding */
+
+SDL_Color *background_color = &white;
+SDL_Color *digit_color      = &white;
+SDL_Color *preset_color     = &dark_green;
+SDL_Color *clickable_color  = &light_green;
+SDL_Color *hovered_color    = &orange;
+SDL_Color *timer_color      = &black;
+
+/* Main GUI Element, Button */
+
+enum state {
+        IDLE,
+        HOVERED,
+        CLICKED
+};
+
+struct button {
+        int x, y, val;
+        enum state state;
+};
+
+#define ROWS 9
+#define COLS 9
+
+struct button buttons[ROWS * COLS];
+
+static void init_buttons(
+        struct button *buttons
+) {
+        int x, y;
+
+        for (y = 0; y < ROWS; ++y)
+                for (x = 0; x < COLS; ++x) {
+                        buttons->x = x;
+                        buttons->y = y;
+                        buttons->val = 1;
+                        buttons->state = IDLE;
+                        ++buttons;
+                }
+}
+
+static int is_point_in_rect(
+        int x,
+        int y,
+        SDL_Rect *r
+) {
+        return x >= r->x
+                && x <= r->x + r->w
+                && y >= r->y
+                && y <= r->y + r->h;
+}
+
+/* Utility Wrappers to Simplify SDL for Myself */
+
+static void set_draw_color(
+        SDL_Renderer *r,
+        SDL_Color *c
+) {
+        SDL_SetRenderDrawColor(r, c->r, c->g, c->b, c->a);
+}
+
 int WinMain(
         HINSTANCE hInstance,
         HINSTANCE hPrevInstance,
@@ -16,12 +118,12 @@ int WinMain(
         SDL_Window *window;
         SDL_Renderer *renderer;
         SDL_Rect rect, dstrect;
-        SDL_Color color;
+        SDL_Color *color;
         SDL_Event event;
         SDL_Surface *text;
         SDL_Texture *texture;
         TTF_Font *font;
-        int x, y, mouse_x, mouse_y, mouse_bt, w, h;
+        int i, x, y, mouse_x, mouse_y, mouse_bt, w, h;
         int res = 0, quit = 0;
 
         if (SDL_Init(SDL_INIT_VIDEO)) {
@@ -59,21 +161,26 @@ int WinMain(
                 goto cleanup_renderer;
         }
 
+        buffer_digits(renderer, font, digit_color, digits_cache);
+        init_buttons(buttons);
+
         while (!quit) {
                 while (SDL_PollEvent(&event)) {
                         if (event.type == SDL_QUIT)
                                 quit = 1;
                         if (event.type == SDL_KEYDOWN)
-                                quit = 1;
+                                if (event.key.keysym.sym == SDLK_ESCAPE)
+                                        quit = 1;
                 }
 
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0);
+                set_draw_color(renderer, background_color);
                 SDL_RenderClear(renderer);
 
                 mouse_bt = SDL_GetMouseState(&mouse_x, &mouse_y);
 
-                for (x = 0; x < 9; ++x)
-                        for (y = 0; y < 9; ++y) {
+                rect.w = rect.h = TILE_SIZE;
+                for (y = 0; y < ROWS; ++y)
+                        for (x = 0; x < COLS; ++x) {
                                 rect.x = x * (TILE_SIZE + TILE_GAP);
                                 if (x >= 3)
                                         rect.x += TILE_GAP * 2;
@@ -84,27 +191,26 @@ int WinMain(
                                         rect.y += TILE_GAP * 2;
                                 if (y >= 6)
                                         rect.y += TILE_GAP * 2;
-                                rect.w = rect.h = TILE_SIZE;
 
-                                SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0xFF, 0);
-
-                                if (mouse_x >= rect.x && mouse_x <= rect.x + rect.w && mouse_y >= rect.y && mouse_y <= rect.y + rect.h)
-                                        if (SDL_BUTTON(mouse_bt) == SDL_BUTTON_LEFT)
-                                                SDL_SetRenderDrawColor(renderer, 0, 0, 0xFF, 0);
-                                        else
-                                                SDL_SetRenderDrawColor(renderer, 0, 0xFF, 0, 0);
-
+                                set_draw_color(renderer, clickable_color);
+                                if (is_point_in_rect(mouse_x, mouse_y, &rect))
+                                        set_draw_color(renderer, hovered_color);
                                 SDL_RenderFillRect(renderer, &rect);
+
+                                if (buttons[y + x].val > 0) {
+                                        SDL_Texture *tex = digits_cache[buttons[y + x].val - 1];
+                                        SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+                                        dstrect = (SDL_Rect) { rect.x, rect.y, w, h };
+                                        SDL_RenderCopy(renderer, tex, NULL, &dstrect);
+                                }
                         }
 
-                color = (SDL_Color) { 0, 0, 0, 0 };
-                text = TTF_RenderUTF8_Blended(font, "Hello, Sudoku!", color);
+                text = TTF_RenderUTF8_Blended(font, "00:00", *timer_color);
                 texture = SDL_CreateTextureFromSurface(renderer, text);
 
                 SDL_QueryTexture(texture, NULL, NULL, &w, &h);
                 dstrect = (SDL_Rect) { 0, 0, w, h };
 
-                SDL_RenderFillRect(renderer, &rect);
                 SDL_RenderCopy(renderer, texture, NULL, &dstrect);
 
                 SDL_DestroyTexture(texture);
@@ -112,6 +218,8 @@ int WinMain(
 
                 SDL_RenderPresent(renderer);
         }
+
+        destroy_digits(digits_cache);
 
         TTF_CloseFont(font);
 cleanup_renderer:
