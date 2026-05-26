c5873fdf717c3134a408f71a2a5b029753abec2c
Anton Muravev <tsb99x@gmail.com>
2026-05-27 00:44:12 +0300

    Condense all of javascript into script.js

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+Element.prototype.clear = function () {
+	while (this.firstChild)
+		this.removeChild(this.firstChild);
+
+	return this;
+};
+
+Element.prototype.addTag = function (tagNameString, namespaceString) {
+	return this.appendChild(
+		this.ownerDocument.createElementNS(namespaceString, tagNameString)
+	);
+};
+
+Element.prototype.addListener = function (eventNameString, callbackFunction, bubbleBoolean) { // addListener ?
+	this.addEventListener(eventNameString, callbackFunction, bubbleBoolean);
+
+	return this;
+};
+
+Element.prototype.setAttributes = function (attributesObject) {
+	var applyAttribute = function (attributeKey) {
+		this.setAttribute(attributeKey, attributesObject[attributeKey]);
+	};
+
+	Object.getOwnPropertyNames(attributesObject)
+		.forEach(applyAttribute, this);
+
+	return this;
+};
+
+Element.prototype.setText = function (string) {
+	this.textContent = string;
+
+	return this;
+};
+
+/* ==================== */
+/* NAMESPACE DEFINITION */
+/* ==================== */
+
+var Utility = Utility || {};
+
+/* ================= */
+/* MODULE DEFINITION */
+/* ================= */
+
+Utility.Misc = (function () {
+
+	/* ============== */
+	/* INTERPRET MODE */
+	/* ============== */
+
+	'use strict';
+
+	/* ================ */
+	/* MODULE INTERFACE */
+	/* ================ */
+
+	return {
+
+		preventDefault: function (event) {
+			event.preventDefault();
+
+			return false;
+		},
+
+		toArray: function (arrayLike) {
+			return Array.prototype.slice.call(arrayLike);
+		}
+
+	};
+
+})();
+
 var App = (function () {
 
 	/* MODE */
